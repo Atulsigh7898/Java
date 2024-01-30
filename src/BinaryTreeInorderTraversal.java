0197eaa93fb874c0e59d416a2736eda7c778a8a2
Atulsigh7898 <143436454+Atulsigh7898@users.noreply.github.com>
2024-01-30 16:02:41 +0530

    changes

---
+++ src/BinaryTreeInorderTraversal.java
@@ -0,0 +1,35 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class BinaryTreeInorderTraversal {
+    public static void inorderTraversal(TreeNode root) {
+        if (root != null) {
+            inorderTraversal(root.left);
+            System.out.print(root.val + " ");
+            inorderTraversal(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        // Example usage:
+        // Construct a binary tree
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        // Perform inorder traversal
+        System.out.println("Inorder Traversal:");
+        inorderTraversal(root);
+    }
+}
